4160b83ef54a4251764e9a4a163304324617fb0d
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-06-24 01:11:15 -0300

    Estilizando um pouco o header(P.S: Como teste apenas)

--- src/Components/Header/style.ts
+++ src/Components/Header/style.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
-color: red;
+width: 100%;
+display: flex;
+justify-content: center;
+background-color: #131214;
+border-bottom: 0.5rem solid #3f3f3f;
+
+svg {
+    max-width: 400px;
+}
+
+@media(max-width: 600px){
+    svg {
+        max-width: 300px;
+    }
+}
 `
