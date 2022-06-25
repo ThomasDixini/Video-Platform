5238a75adf840ff246ddd41b3454ce7dd6550960
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-06-25 13:48:44 -0300

    Criando página e renderizando em tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { Header } from "./Components/Header/Header";
-import { client } from "./lib/apollo"
+import { Event } from "./pages/Event";
 import { GlobalStyle } from "./styles/global";
-
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id,
-      title
-    }
-  }
-`
-interface Lesson {
-  id: string;
-  title: string;
-}
 
 export function App() {
 
-  const { data } = useQuery<{ lessons: Lesson[]}>(GET_LESSONS_QUERY);
-
   return (
     <>
-      <Header />
+      <Event />
       <GlobalStyle />
     </>
   )
