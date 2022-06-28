b79f19831beb19be59f7c834c4db376d31281253
Thomás Dixini <83423919+ThomasDixini@users.noreply.github.com>
2022-06-27 23:46:23 -0300

    Criando estrutura do html, e dando a primeira estilizada

--- src/Components/Video/Video.tsx
+++ src/Components/Video/Video.tsx
@@ -1,9 +1,34 @@
 import { Container } from "./style";
 
+import { TbBrandDiscord } from 'react-icons/tb'
+import { BsLightningCharge } from 'react-icons/bs'
+
 export function Video(){
     return(
         <Container>
-            Video
+            <div className="bg-player">
+                <div className="player">
+                    Video
+                </div>
+            </div>
+
+            <div className="content-lesson">
+                <div className="description">
+                    <span> Aula 01 - Abertura do Evento Ignite Lab</span>
+                    <p>
+                        Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.               
+                    </p>
+                </div>
+                <div className="link-buttons">
+                    <a href="#">
+                        <TbBrandDiscord size={24} /> Comunidade do Discord
+                    </a>
+                    <a href="#" className="btn-challenge">
+                       <BsLightningCharge size={24} /> Acesse o desafio
+                    </a>
+                </div>
+            </div>
+            
         </Container>
     );
 }
--- src/Components/Video/style.ts
+++ src/Components/Video/style.ts
@@ -4,4 +4,89 @@ export const Container = styled.div`
 
 display: flex;
 flex: 1 1 0%;
+flex-direction: column;
+
+
+div.bg-player {
+    background-color: black;
+    width: 100%;
+    height: 100%;
+    max-height: 60vh;
+    aspect-ratio: 16/9;
+    display: flex;
+    justify-content: center;
+
+    div.player {
+        width: 100%;
+        max-width: 1100px;
+        max-height: 60vh;
+        aspect-ratio: 16/9;
+        background-color: gray;
+    }
+}
+
+div.content-lesson {
+    max-width: 1100px;
+    width: 100%;
+    margin: 0 auto;
+    padding: 2rem;
+
+    display: flex;
+    align-items: flex-start;
+
+    div.description {
+        span {
+            font-size: 1.5rem;
+            font-weight: bold;
+        }
+        p {
+            font-size: 1rem;
+            margin-top: 2rem;
+            line-height: 1.65rem;
+            color: var(--gray-300);
+        }
+    }
+
+    div.link-buttons {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        margin-left: 4rem;
+        a {
+            background-color: var(--blue-700);
+            padding: 1.25rem;
+            border-radius: 0.25rem;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            text-transform: uppercase;
+            font-weight: bold;
+            transition: filter 0.2s;
+
+            & {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                gap: 0.5rem;
+            }
+
+            &:hover {
+                filter: brightness(0.75);
+            }
+
+            &.btn-challenge {
+                background-color: transparent;
+                border: 2px solid var(--green);
+                color: var(--green);
+
+                &:hover {
+                    background-color: var(--green);
+                    color: black;
+                    filter: brightness(1);
+                }
+            }
+        }
+    }
+}
 `
